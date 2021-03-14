c11088af29f29e3989335fdd2c442e8b6e0f07fc
阿七 <yiqiujia@hotmail.com>
2021-03-14 09:34:02 +0800

    test

--- test5.js
+++ test5.js
@@ -1,15 +1,23 @@
 // @1ts-check
-const playwright = require('playwright');
+const { chromium } = require('playwright');
+const { saveVideo } = require('playwright-video');
+const path = require('path');
 
 (async () => {
-  const browser = await playwright.webkit.launch();
+  const timeout = function(ms){
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+  const browser = await chromium.launch();
   const context = await browser.newContext();
   const page = await context.newPage();
+  await saveVideo(page, __dirname + path.sep +'video.mp4');
   page.route('**', (route, request) => {
     console.log(request.url());
     route.continue();
   });
   await page.goto('https://www.baidu.com');
-  await page.screenshot({ path: `example.png` });
+  await timeout(5000);
+  await page.screenshot({ path: __dirname + path.sep + `example.png` , fullPage: true});
+  console.log('save example.png');
   await browser.close();
 })();
